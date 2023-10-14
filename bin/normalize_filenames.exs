b13b461025bb22d14d84a1b7fead306750366d00
Susumu Yamazaki <zacky1972@gmail.com>
2023-10-14 12:41:59 +0900

    enhance: implement update function by torifukukaiou

--- bin/normalize_filenames.exs
+++ bin/normalize_filenames.exs
@@ -22,7 +22,7 @@ end
 defmodule NormalizeFilename do
   def normalize(filename) do
     case Regex.match?(~r/^\d{8}-.+\.md$/, filename) do
-      true -> filename
+      true -> maybe_do_normalize(filename)
       false -> do_normalize(filename)
     end
   end
@@ -35,21 +35,46 @@ defmodule NormalizeFilename do
       |> Timex.parse!("{ISO:Extended}")
       |> Timex.format!("%Y%m%d", :strftime)
 
-    normalized_title =
-      Zarex.sanitize(title)
-      |> then(&Regex.replace(~r/\[/, &1, "【"))
-      |> then(&Regex.replace(~r/\]/, &1, "】"))
-      |> String.slice(0, 80)
+    normalized_title = normalized_title(title)
 
-    "#{created_at}-#{normalized_title}.md"
+    build_filename(created_at, normalized_title)
   end
 
   defp do_normalize(filename) do
-    path = Path.join(File.cwd!(), "public") |> Path.join(filename)
-
-    {%{"id" => id, "title" => title}, _body} = YamlFrontMatter.parse_file!(path)
+    {id, title} = filename |> path() |> parse_file()
     do_normalize(id, title, filename)
   end
+
+  defp maybe_do_normalize(title, title, _date, filename), do: filename
+
+  defp maybe_do_normalize(normalized_title, _filename_title, date, _filename),
+    do: build_filename(date, normalized_title)
+
+  defp maybe_do_normalize(filename) do
+    {_id, raw_title} = filename |> path() |> parse_file()
+    normalized_title = normalized_title(raw_title)
+
+    %{"date" => date, "title" => filename_title} =
+      Regex.named_captures(~r/^(?<date>\d{8})-(?<title>.+)\.md$/, filename)
+
+    maybe_do_normalize(normalized_title, filename_title, date, filename)
+  end
+
+  defp path(filename), do: Path.join(File.cwd!(), "public") |> Path.join(filename)
+
+  defp parse_file(path) do
+    {%{"id" => id, "title" => title}, _body} = YamlFrontMatter.parse_file!(path)
+    {id, title}
+  end
+
+  defp normalized_title(title) do
+    Zarex.sanitize(title)
+    |> then(&Regex.replace(~r/\[/, &1, "【"))
+    |> then(&Regex.replace(~r/\]/, &1, "】"))
+    |> String.slice(0, 80)
+  end
+
+  defp build_filename(created_at, normalized_title), do: "#{created_at}-#{normalized_title}.md"
 end
 
 defmodule Main do
